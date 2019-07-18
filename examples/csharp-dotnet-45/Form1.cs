bb9ab15b52e5046fcb2388266ca21c2e4727a6ad
adnan-kamili <adnan.kamili@gmail.com>
2019-07-18 06:26:45 +0000

    updated examples

--- examples/csharp-dotnet-45/Form1.cs
+++ examples/csharp-dotnet-45/Form1.cs
@@ -10,126 +10,109 @@ namespace Sample
         public ActivationForm()
         {
             InitializeComponent();
-            int status;
-            // status = LexActivator.SetProductFile ("ABSOLUTE_PATH_OF_PRODUCT.DAT_FILE");
-            status = LexActivator.SetProductData("PASTE_CONTENT_OF_PRODUCT.DAT_FILE");
-            if (status != LexActivator.StatusCodes.LA_OK)
+            try
             {
-                this.statusLabel.Text = "Error setting product file: " + status.ToString();
-                return;
-            }
-            status = LexActivator.SetProductId("PASTE_PRODUCT_ID", LexActivator.PermissionFlags.LA_USER);
-            if (status != LexActivator.StatusCodes.LA_OK)
-            {
-                this.statusLabel.Text = "Error setting product id: " + status.ToString();
-                return;
-            }
-            // Setting license callback is recommended for floating licenses
-            // status = LexActivator.SetLicenseCallback(LicenseCallback);
-            // if (status != LexActivator.StatusCodes.LA_OK)
-            // {
-            // this.statusLabel.Text = "Error setting callback function: " + status.ToString();
-            // return;
-            // }
-            status = LexActivator.IsLicenseGenuine();
-            if (status == LexActivator.StatusCodes.LA_OK || status == LexActivator.StatusCodes.LA_EXPIRED || status == LexActivator.StatusCodes.LA_SUSPENDED || status == LexActivator.StatusCodes.LA_GRACE_PERIOD_OVER)
-            {
-                // uint expiryDate = 0;
-                // LexActivator.GetLicenseExpiryDate(ref expiryDate);
-                // int daysLeft = (int)(expiryDate - unixTimestamp()) / 86400;
-                this.statusLabel.Text = "License genuinely activated! Activation Status: " + status.ToString();
-                this.activateBtn.Text = "Deactivate";
-                this.activateTrialBtn.Enabled = false;
+                // LexActivator.SetProductFile ("ABSOLUTE_PATH_OF_PRODUCT.DAT_FILE");
+                LexActivator.SetProductData("PASTE_CONTENT_OF_PRODUCT.DAT_FILE");
+                LexActivator.SetProductId("PASTE_PRODUCT_ID", LexActivator.PermissionFlags.LA_USER);
+                // LexActivator.SetLicenseCallback(LicenseCallback);
 
-                // Checking for software release update
-                // status = LexActivator.CheckForReleaseUpdate("windows", "1.0.0", "stable", SoftwareReleaseUpdateCallback);
-                // if (status != LexActivator.StatusCodes.LA_OK)
-                // {
-                //     this.statusLabel.Text = "Error checking for software release update: " + status.ToString();
-                // }
-                return;
+                int status = LexActivator.IsLicenseGenuine();
+                if (status == LexStatusCodes.LA_OK || status == LexStatusCodes.LA_EXPIRED || status == LexStatusCodes.LA_SUSPENDED || status == LexStatusCodes.LA_GRACE_PERIOD_OVER)
+                {
+                    uint expiryDate = LexActivator.GetLicenseExpiryDate();
+                    int daysLeft = (int)(expiryDate - unixTimestamp()) / 86400;
+                    this.statusLabel.Text = "License genuinely activated! Activation Status: " + status.ToString();
+                    this.activateBtn.Text = "Deactivate";
+                    this.activateTrialBtn.Enabled = false;
+
+                    // Checking for software release update
+                    // LexActivator.CheckForReleaseUpdate("windows", "1.0.0", "stable", SoftwareReleaseUpdateCallback);
+                    return;
+                }
+                status = LexActivator.IsTrialGenuine();
+                if (status == LexStatusCodes.LA_OK)
+                {
+                    uint trialExpiryDate = LexActivator.GetTrialExpiryDate();
+                    int daysLeft = (int)(trialExpiryDate - unixTimestamp()) / 86400;
+                    this.statusLabel.Text = "Trial period! Days left:" + daysLeft.ToString();
+                    this.activateTrialBtn.Enabled = false;
+                }
+                else if (status == LexStatusCodes.LA_TRIAL_EXPIRED)
+                {
+                    this.statusLabel.Text = "Trial has expired!";
+                }
+                else
+                {
+                    this.statusLabel.Text = "Trial has not started or has been tampered: " + status.ToString();
+                }
             }
-            status = LexActivator.IsTrialGenuine();
-            if (status == LexActivator.StatusCodes.LA_OK)
+            catch (LexActivatorException ex)
             {
-                uint trialExpiryDate = 0;
-                LexActivator.GetTrialExpiryDate(ref trialExpiryDate);
-                int daysLeft = (int)(trialExpiryDate - unixTimestamp()) / 86400;
-                this.statusLabel.Text = "Trial period! Days left:" + daysLeft.ToString();
-                this.activateTrialBtn.Enabled = false;
-            }
-            else if (status == LexActivator.StatusCodes.LA_TRIAL_EXPIRED)
-            {
-                this.statusLabel.Text = "Trial has expired!";
-            }
-            else
-            {
-                this.statusLabel.Text = "Trial has not started or has been tampered: " + status.ToString();
+                this.statusLabel.Text = "Error code: " + ex.Code.ToString() + " Error message: " + ex.Message;
             }
         }
 
         private void activateBtn_Click(object sender, EventArgs e)
         {
-            int status;
-            if (this.activateBtn.Text == "Deactivate")
+            try
             {
-                status = LexActivator.DeactivateLicense();
-                if (status == LexActivator.StatusCodes.LA_OK)
+                int status;
+                if (this.activateBtn.Text == "Deactivate")
                 {
-                    this.statusLabel.Text = "License deactivated successfully";
-                    this.activateBtn.Text = "Activate";
-                    this.activateTrialBtn.Enabled = true;
+                    status = LexActivator.DeactivateLicense();
+                    if (status == LexStatusCodes.LA_OK)
+                    {
+                        this.statusLabel.Text = "License deactivated successfully";
+                        this.activateBtn.Text = "Activate";
+                        this.activateTrialBtn.Enabled = true;
+                        return;
+                    }
+                    this.statusLabel.Text = "Error deactivating license: " + status.ToString();
                     return;
                 }
-                this.statusLabel.Text = "Error deactivating license: " + status.ToString();
-                return;
+                LexActivator.SetLicenseKey(productKeyBox.Text);
+                LexActivator.SetActivationMetadata("key1", "value1");
+                status = LexActivator.ActivateLicense();
+                if (status == LexStatusCodes.LA_OK || status == LexStatusCodes.LA_EXPIRED || status == LexStatusCodes.LA_SUSPENDED)
+                {
+                    this.statusLabel.Text = "Activation Successful :" + status.ToString();
+                    this.activateBtn.Text = "Deactivate";
+                    this.activateTrialBtn.Enabled = false;
+                }
+                else
+                {
+                    this.statusLabel.Text = "Error activating the license: " + status.ToString();
+                    return;
+                }
             }
-            status = LexActivator.SetLicenseKey(productKeyBox.Text);
-            if (status != LexActivator.StatusCodes.LA_OK)
+            catch (LexActivatorException ex)
             {
-                this.statusLabel.Text = "Error setting license key: " + status.ToString();
-                return;
+                this.statusLabel.Text = "Error code: " + ex.Code.ToString() + " Error message: " + ex.Message;
             }
-            status = LexActivator.SetActivationMetadata("key1", "value1");
-            if (status != LexActivator.StatusCodes.LA_OK)
-            {
-                this.statusLabel.Text = "Error setting activation metadata: " + status.ToString();
-                return;
-            }
-            status = LexActivator.ActivateLicense();
-            if (status == LexActivator.StatusCodes.LA_OK || status == LexActivator.StatusCodes.LA_EXPIRED || status == LexActivator.StatusCodes.LA_SUSPENDED)
-            {
-                this.statusLabel.Text = "Activation Successful :" + status.ToString();
-                this.activateBtn.Text = "Deactivate";
-                this.activateTrialBtn.Enabled = false;
-            }
-            else
-            {
-                this.statusLabel.Text = "Error activating the license: " + status.ToString();
-                return;
-            }
-
         }
 
         private void activateTrialBtn_Click(object sender, EventArgs e)
         {
-            int status;
-            status = LexActivator.SetTrialActivationMetadata("key2", "value2");
-            if (status != LexActivator.StatusCodes.LA_OK)
+            try
             {
-                this.statusLabel.Text = "Error setting activation metadata: " + status.ToString();
-                return;
+                LexActivator.SetTrialActivationMetadata("key2", "value2");
+                int status = LexActivator.ActivateTrial();
+                if (status != LexStatusCodes.LA_OK)
+                {
+                    this.statusLabel.Text = "Error activating the trial: " + status.ToString();
+                    return;
+                }
+                else
+                {
+                    this.statusLabel.Text = "Trial started Successful";
+                }
             }
-            status = LexActivator.ActivateTrial();
-            if (status != LexActivator.StatusCodes.LA_OK)
+            catch (LexActivatorException ex)
             {
-                this.statusLabel.Text = "Error activating the trial: " + status.ToString();
-                return;
+                this.statusLabel.Text = "Error code: " + ex.Code.ToString() + " Error message: " + ex.Message;
             }
-            else
-            {
-                this.statusLabel.Text = "Trial started Successful";
-            }
+            
         }
 
         // License callback is invoked when LexActivator.IsLicenseGenuine() completes a server sync
@@ -138,7 +121,7 @@ namespace Sample
             // NOTE: Don't invoke IsLicenseGenuine(), ActivateLicense() or ActivateTrial() API functions in this callback
             switch (status)
             {
-                case LexActivator.StatusCodes.LA_SUSPENDED:
+                case LexStatusCodes.LA_SUSPENDED:
                     this.statusLabel.Text = "The license has been suspended.";
                     break;
                 default:
@@ -152,10 +135,10 @@ namespace Sample
         {
             switch (status)
             {
-                case LexActivator.StatusCodes.LA_RELEASE_UPDATE_AVAILABLE:
+                case LexStatusCodes.LA_RELEASE_UPDATE_AVAILABLE:
                     this.statusLabel.Text = "An update is available for the app.";
                     break;
-                case LexActivator.StatusCodes.LA_RELEASE_NO_UPDATE_AVAILABLE:
+                case LexStatusCodes.LA_RELEASE_NO_UPDATE_AVAILABLE:
                     // Current version is already latest.
                     break;
                 default:
--- examples/vb-dotnet-45/Form1.vb
+++ examples/vb-dotnet-45/Form1.vb
@@ -1,115 +1,106 @@
-﻿Imports System.IO
-Imports Sample.Cryptlex
+﻿Imports Cryptlex
+
 
 Public Class Form1
     Public Sub New()
 
         InitializeComponent()
-        Dim status As Integer
-        status = LexActivator.SetProductData("PASTE_CONTENT_OF_PRODUCT.DAT_FILE")
-        If status <> LexActivator.StatusCodes.LA_OK Then
-            Me.statusLabel.Text = "Error setting product file: " & status.ToString()
-            Return
-        End If
 
-        status = LexActivator.SetProductId("PASTE_PRODUCT_ID", LexActivator.PermissionFlags.LA_USER)
-        If status <> LexActivator.StatusCodes.LA_OK Then
-            Me.statusLabel.Text = "Error setting product id: " & status.ToString()
-            Return
-        End If
-        ' Setting license callback is recommended for floating licenses
-        'status = LexActivator.SetLicenseCallback(AddressOf LicenseCallback)
-        'If status <> LexActivator.StatusCodes.LA_OK Then
-        '    Me.statusLabel.Text = "Error setting callback function: " & status.ToString()
-        '    Return
-        'End If
-        status = LexActivator.IsLicenseGenuine()
-        If status = LexActivator.StatusCodes.LA_OK OrElse status = LexActivator.StatusCodes.LA_EXPIRED OrElse status = LexActivator.StatusCodes.LA_SUSPENDED OrElse status = LexActivator.StatusCodes.LA_GRACE_PERIOD_OVER Then
-            Me.statusLabel.Text = "License genuinely activated! Activation Status: " & status.ToString()
-            Me.activateBtn.Text = "Deactivate"
-            Me.activateTrialBtn.Enabled = False
-            ' Checking for software release update...
-            ' status = LexActivator.CheckForReleaseUpdate("windows", "1.0.0", "stable", AddressOf SoftwareReleaseUpdateCallback)
-            ' If status <> LexActivator.StatusCodes.LA_OK Then
-            '   Me.statusLabel.Text = "Error checking for software release update: " & status.ToString()
-            ' End If
-            Return
-        End If
+        Try
+            'LexActivator.SetProductFile ("ABSOLUTE_PATH_OF_PRODUCT.DAT_FILE")
+            LexActivator.SetProductData("PASTE_CONTENT_OF_PRODUCT.DAT_FILE")
+            LexActivator.SetProductId("PASTE_PRODUCT_ID", LexActivator.PermissionFlags.LA_USER)
+            'LexActivator.SetLicenseCallback(AddressOf LicenseCallback)
+            Dim status As Integer = LexActivator.IsLicenseGenuine()
 
-        status = LexActivator.IsTrialGenuine()
-        If status = LexActivator.StatusCodes.LA_OK Then
-            Dim trialExpiryDate As UInteger = 0
-            LexActivator.GetTrialExpiryDate(trialExpiryDate)
-            Dim daysLeft As Integer = CInt((trialExpiryDate - unixTimestamp())) / 86400
-            Me.statusLabel.Text = "Trial period! Days left:" & daysLeft.ToString()
-            Me.activateTrialBtn.Enabled = False
-        ElseIf status = LexActivator.StatusCodes.LA_TRIAL_EXPIRED Then
-            Me.statusLabel.Text = "Trial has expired!"
-        Else
-            Me.statusLabel.Text = "Trial has not started or has been tampered: " & status.ToString()
-        End If
+            If status = LexStatusCodes.LA_OK OrElse status = LexStatusCodes.LA_EXPIRED OrElse status = LexStatusCodes.LA_SUSPENDED OrElse status = LexStatusCodes.LA_GRACE_PERIOD_OVER Then
+                Dim expiryDate As UInteger = LexActivator.GetLicenseExpiryDate()
+                Dim daysLeft As Integer = CInt((expiryDate - unixTimestamp())) / 86400
+                Me.statusLabel.Text = "License genuinely activated! Activation Status: " & status.ToString()
+                Me.activateBtn.Text = "Deactivate"
+                Me.activateTrialBtn.Enabled = False
+
+                'Checking for software release update
+                'LexActivator.CheckForReleaseUpdate("windows", "1.0.0", "stable", AddressOf SoftwareReleaseUpdateCallback)
+                Return
+            End If
+
+            status = LexActivator.IsTrialGenuine()
+
+            If status = LexStatusCodes.LA_OK Then
+                Dim trialExpiryDate As UInteger = LexActivator.GetTrialExpiryDate()
+                Dim daysLeft As Integer = CInt((trialExpiryDate - unixTimestamp())) / 86400
+                Me.statusLabel.Text = "Trial period! Days left:" & daysLeft.ToString()
+                Me.activateTrialBtn.Enabled = False
+            ElseIf status = LexStatusCodes.LA_TRIAL_EXPIRED Then
+                Me.statusLabel.Text = "Trial has expired!"
+            Else
+                Me.statusLabel.Text = "Trial has not started or has been tampered: " & status.ToString()
+            End If
+
+        Catch ex As LexActivatorException
+            Me.statusLabel.Text = "Error code: " & ex.Code.ToString() & " Error message: " + ex.Message
+        End Try
 
     End Sub
 
     Public Sub activateBtn_Click(sender As Object, e As EventArgs) Handles activateBtn.Click
-        Dim status As Integer
-        If Me.activateBtn.Text = "Deactivate" Then
-            status = LexActivator.DeactivateLicense()
-            If status = LexActivator.StatusCodes.LA_OK Then
-                Me.statusLabel.Text = "License deactivated successfully"
-                Me.activateBtn.Text = "Activate"
-                Me.activateTrialBtn.Enabled = True
+        Try
+            Dim status As Integer
+
+            If Me.activateBtn.Text = "Deactivate" Then
+                status = LexActivator.DeactivateLicense()
+
+                If status = LexStatusCodes.LA_OK Then
+                    Me.statusLabel.Text = "License deactivated successfully"
+                    Me.activateBtn.Text = "Activate"
+                    Me.activateTrialBtn.Enabled = True
+                    Return
+                End If
+
+                Me.statusLabel.Text = "Error deactivating license: " & status.ToString()
                 Return
             End If
 
-            Me.statusLabel.Text = "Error deactivating license: " & status.ToString()
-            Return
-        End If
+            LexActivator.SetLicenseKey(productKeyBox.Text)
+            LexActivator.SetActivationMetadata("key1", "value1")
+            status = LexActivator.ActivateLicense()
 
-        status = LexActivator.SetLicenseKey(productKeyBox.Text)
-        If status <> LexActivator.StatusCodes.LA_OK Then
-            Me.statusLabel.Text = "Error setting license key: " & status.ToString()
-            Return
-        End If
+            If status = LexStatusCodes.LA_OK OrElse status = LexStatusCodes.LA_EXPIRED OrElse status = LexStatusCodes.LA_SUSPENDED Then
+                Me.statusLabel.Text = "Activation Successful :" & status.ToString()
+                Me.activateBtn.Text = "Deactivate"
+                Me.activateTrialBtn.Enabled = False
+            Else
+                Me.statusLabel.Text = "Error activating the license: " & status.ToString()
+                Return
+            End If
 
-        status = LexActivator.SetActivationMetadata("key1", "value1")
-        If status <> LexActivator.StatusCodes.LA_OK Then
-            Me.statusLabel.Text = "Error setting activation metadata: " & status.ToString()
-            Return
-        End If
-        
-        status = LexActivator.ActivateLicense()
-        If status = LexActivator.StatusCodes.LA_OK OrElse status = LexActivator.StatusCodes.LA_EXPIRED OrElse status = LexActivator.StatusCodes.LA_SUSPENDED Then
-            Me.statusLabel.Text = "Activation Successful: " & status.ToString()
-            Me.activateBtn.Text = "Deactivate"
-            Me.activateTrialBtn.Enabled = False
-        Else
-            Me.statusLabel.Text = "Error activating the license: " & status.ToString()
-            Return
-        End If
+        Catch ex As LexActivatorException
+            Me.statusLabel.Text = "Error code: " & ex.Code.ToString() & " Error message: " + ex.Message
+        End Try
     End Sub
 
     Public Sub activateTrialBtn_Click(sender As Object, e As EventArgs) Handles activateTrialBtn.Click
-        Dim status As Integer
-        status = LexActivator.SetTrialActivationMetadata("key2", "value2")
-        If status <> LexActivator.StatusCodes.LA_OK Then
-            Me.statusLabel.Text = "Error setting activation metadata: " & status.ToString()
-            Return
-        End If
+        Try
+            LexActivator.SetTrialActivationMetadata("key2", "value2")
+            Dim status As Integer = LexActivator.ActivateTrial()
 
-        status = LexActivator.ActivateTrial()
-        If status <> LexActivator.StatusCodes.LA_OK Then
-            Me.statusLabel.Text = "Error activating the trial: " & status.ToString()
-            Return
-        Else
-            Me.statusLabel.Text = "Trial started Successful"
-        End If
+            If status <> LexStatusCodes.LA_OK Then
+                Me.statusLabel.Text = "Error activating the trial: " & status.ToString()
+                Return
+            Else
+                Me.statusLabel.Text = "Trial started Successful"
+            End If
+
+        Catch ex As LexActivatorException
+            Me.statusLabel.Text = "Error code: " & ex.Code.ToString() & " Error message: " + ex.Message
+        End Try
     End Sub
 
     Private Sub LicenseCallback(ByVal status As UInteger)
         ' NOTE: Don't invoke IsLicenseGenuine(), ActivateLicense() or ActivateTrial() API functions in this callback
         Select Case status
-            Case LexActivator.StatusCodes.LA_SUSPENDED
+            Case LexStatusCodes.LA_SUSPENDED
                 Me.statusLabel.Text = "The license has been suspended."
             Case Else
                 Me.statusLabel.Text = "License status code: " & status.ToString()
@@ -122,9 +113,9 @@ Public Class Form1
 
     Private Sub SoftwareReleaseUpdateCallback(ByVal status As UInteger)
         Select Case status
-            Case LexActivator.StatusCodes.LA_RELEASE_UPDATE_AVAILABLE
+            Case LexStatusCodes.LA_RELEASE_UPDATE_AVAILABLE
                 Me.statusLabel.Text = "An update is available for the app."
-            Case LexActivator.StatusCodes.LA_RELEASE_NO_UPDATE_AVAILABLE
+            Case LexStatusCodes.LA_RELEASE_NO_UPDATE_AVAILABLE
                 ' Current version is latest
             Case Else
                 Me.statusLabel.Text = "Release status code: " & status.ToString()
