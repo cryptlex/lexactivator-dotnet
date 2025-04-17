bf1e2dc617be3694eeb3c1723fba39f15fda911e
muneebkq <muneebkq05@gmail.com>
2025-04-17 08:44:04 +0530

    chore: add feature entitlement class

---
+++ src/Cryptlex.LexActivator/FeatureEntitlement.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptlex
+{
+    public class FeatureEntitlement
+    {
+        /// <summary>
+        /// Feature entitlement name.
+        /// </summary>
+        public string FeatureName;
+
+        /// <summary>
+        /// Feature entitlement value.
+        /// </summary>
+        public string Value;
+    
+    }
+}
