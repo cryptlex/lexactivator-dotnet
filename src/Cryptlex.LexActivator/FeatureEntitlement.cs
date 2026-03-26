7a40f741457b29ffa033c0324ab263ea7eb55e51
Huzaif Mushtaq Mir <huzaif@Huzaif-MacBook-Air.local>
2026-03-26 13:54:49 +0530

    chore: update doctrings

--- src/Cryptlex.LexActivator/FeatureEntitlement.cs
+++ src/Cryptlex.LexActivator/FeatureEntitlement.cs
@@ -17,12 +17,14 @@ namespace Cryptlex
         public string FeatureDisplayName;
 
         /// <summary>
-        /// The value of the feature
+        /// Effective value of the feature. Contains the overridden value if set at the license level;
+        /// otherwise, the entitlement set value.
         /// </summary>
         public string Value;
 
         /// <summary>
-        /// The default value of the feature inherited from the entitlement set.
+        ///  Default value of the feature defined in the entitlement set; empty for features not inherited
+        /// from an entitlement set.
         /// </summary>
         public string BaseValue;
     
